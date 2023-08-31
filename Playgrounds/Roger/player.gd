c6befd864fb5d4f97c7914b9b9e61812a76efb67
Rogelio De La Garza <rogeliodelagarza3901@gmail.com>
2023-08-31 13:31:18 -0400

    Added hitbox and animation

--- Playgrounds/Roger/player.gd
+++ Playgrounds/Roger/player.gd
@@ -10,13 +10,18 @@ var gold = 0
 
 var input = Vector2.ZERO
 var last_movement = Vector2.ZERO
+var is_attacking = false
 
 func _physics_process(delta):
+	if Input.is_action_pressed("attack1"):
+		is_attacking = true
+	if Input.is_action_just_released("attack1"):
+		is_attacking = false
 	player_movement(delta)
 
 func get_input():
-	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
 
 	if input.x > 0:
 		last_movement = Vector2.RIGHT
@@ -37,19 +42,26 @@ func get_input():
 
 func play_idle_animation():
 	if velocity.length() < idle_threshold:
-		if last_movement == Vector2.RIGHT:
-			get_node("AnimatedSprite2D").play("IdleRight")
-		elif last_movement == Vector2.LEFT:
-			get_node("AnimatedSprite2D").play("IdleLeft")
-		elif last_movement == Vector2.DOWN:
-			get_node("AnimatedSprite2D").play("IdleUp")
-		elif last_movement == Vector2.UP:
-			get_node("AnimatedSprite2D").play("IdleDown")
+		if is_attacking == false:
+			if last_movement == Vector2.RIGHT:
+				get_node("AnimatedSprite2D").play("IdleRight")
+			elif last_movement == Vector2.LEFT:
+				get_node("AnimatedSprite2D").play("IdleLeft")
+			elif last_movement == Vector2.DOWN:
+				get_node("AnimatedSprite2D").play("IdleUp")
+			elif last_movement == Vector2.UP:
+				get_node("AnimatedSprite2D").play("IdleDown")
+		else:
+			sword_attack()
 
 func player_movement(delta):
-	input = get_input()
+	if is_attacking == false:
+		input = get_input()
+	else:
+		input = Vector2.ZERO
 
 	if input == Vector2.ZERO:
+		sword_attack() # this stops the character if walking and attacks
 		if velocity.length() > (friction * delta):
 			velocity -= velocity.normalized() * (friction * delta)
 		else:
@@ -60,3 +72,21 @@ func player_movement(delta):
 
 	move_and_slide()
 
+func sword_attack():
+	if is_attacking == true:
+		if last_movement == Vector2.RIGHT:
+			get_node("AnimatedSprite2D").play("SwordAttackRight")
+			await get_node("AnimatedSprite2D").animation_finished
+			is_attacking = false
+		elif last_movement == Vector2.LEFT:
+			get_node("AnimatedSprite2D").play("SwordAttackLeft")
+			await get_node("AnimatedSprite2D").animation_finished
+			is_attacking = false
+		elif last_movement == Vector2.DOWN:
+			get_node("AnimatedSprite2D").play("SwordAttackUp")
+			await get_node("AnimatedSprite2D").animation_finished
+			is_attacking = false
+		elif last_movement == Vector2.UP:
+			get_node("AnimatedSprite2D").play("SwordAttackDown")
+			await get_node("AnimatedSprite2D").animation_finished
+			is_attacking = false
