d1003fa5d0bb9d76d42cc0fb8706da5d1345d285
Donald Brantley <dcbrantley@hotmail.com>
2023-09-12 10:24:47 -0400

    setup dungeon, made mass skel spawner

--- Props/Coins/Coin_with_Physics/coin_physics.gd
+++ Props/Coins/Coin_with_Physics/coin_physics.gd
@@ -1,10 +1,17 @@
-extends Area2D
+extends RigidBody2D
+
 
 @onready var audio_player = $AudioStreamPlayer2D
 @onready var delay_timer = $Timer
 
+func _ready():
+	# ensure the coin doesn't slide or spin too much.
+	self.linear_damp = 3.0
+	self.angular_damp = 1.0
+	self.gravity_scale = 0.0
+	
 
-func _on_body_entered(body):
+func _on_coin_pickup_area_body_entered(body):
 	if body.name == "Hero":
 		Game.Gold += 5
 		play_pickup_sound()
@@ -12,7 +19,6 @@ func _on_body_entered(body):
 func play_pickup_sound():
 	audio_player.play()
 	delay_timer.start()
-
 
 func _on_timer_timeout():
 	queue_free()
