7805f343a7598f5c341818109d0a8c8fadf9cd22
BrendaLDelgado <brenda.l.delgado94@gmail.com>
2023-09-13 15:03:03 -0500

    removed print statements and water sounds work

--- Maps/Crypt1/Crypt1.gd
+++ Maps/Crypt1/Crypt1.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,6 +12,6 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	print('player has entered scene transition zone')
+	#print('player has entered scene transition zone')
 	if body.name == "Hero":
 		get_tree().change_scene_to_file("res://Maps/Main_World/world_one.tscn") # Replace with function body.
